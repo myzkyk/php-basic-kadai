99eaea93fb19ef7a15f9c8fa7ae3d76133324a27
myzkyk <ykgpno.6@gmail.com>
2025-08-16 13:03:47 +0900

    upload kadai_sort

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,28 +9,38 @@
 <body>
     <p>
         <?php
-        // ここにコードを書いていく
-        $numbers = array(10, 3, 5, 7, 15, 2, 8);
+// 独自のソート関数を定義
+function sort_2way($array, $order) {
+    // 昇順 or 降順を判定
+    if ($order === "ASC") {
+        sort($array); // 昇順
+    } elseif ($order === "DESC") {
+        rsort($array); // 降順
+    } else {
+        echo "ソートの順序指定が不正です。ASC または DESC を指定してください。<br>";
+        return;
+    }
 
+    // 配列を出力（<br>で改行）
+    foreach ($array as $value) {
+        echo $value . "<br>";
+    }
+}
 
-        sort($numbers);
+// テスト用配列
+$numbers = array(10, 3, 5, 7, 15, 2, 8);
 
+// 昇順ソート
+echo "昇順ソートします。<br>";
+sort_2way($numbers, "ASC");
 
-        echo "昇順ソートします。\n";
-        foreach ($numbers as $number) {
-        echo $number . "\n";
-        }
+echo "<br>";
 
-        echo "\n"; 
+// 降順ソート
+echo "降順ソートします。<br>";
+sort_2way($numbers, "DESC");
+?>
 
-
-        rsort($numbers);
-
-        echo "降順ソートします。\n";
-        foreach ($numbers as $number) {
-       echo $number . "\n";
-        }
-        ?>
     </p>
 </body>
 
